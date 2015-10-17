eebd46a0665e18973092cfe2773c731cbd6069fb
msf12 <msf12@hampshire.edu>
2015-10-17 18:30:19 -0400

    Print name of every file in SD card

--- Teensy/sketch_oct17a/sketch_oct17a.ino
+++ Teensy/sketch_oct17a/sketch_oct17a.ino
@@ -2,72 +2,130 @@
 #include <SdFat.h>
 #include <SdFatUtil.h>
 
+#define LFN_LENGTH 160
+
 // sd card chip select pin.
-const uint8_t sd_CS_PIN = SS;
+const uint8_t SD_CS_PIN = SS;
 
 unsigned long baud = 19200;
 SdFat sd;
-SdFile file;
 String path;
 
+void lsDir(bool root = false)
+{
+	SdFile file;
+	char lfn[160]; //char[] to hold the long file names
+	sd.vwd()->getName(lfn,LFN_LENGTH); //store the long file name of the working directory in lfn
+
+	int pathLength = path.length(); //store length of the path to remove added parts later
+	
+	// Serial.print("Current vwd is ");
+	// sd.vwd()->printName(&Serial);
+	Serial.print("\nCurrent path is ");
+
+	if(!root) //root path + lfn would be "//"
+	{
+		Serial.println(path + lfn + "/");
+		// Serial.println("DEBUG: NOT ROOT DIRECTORY");
+		path += lfn;
+		path += "/";
+	}
+	else
+	{
+		Serial.println(path.c_str());
+	}
+
+	SdFile currentDir(path.c_str(),O_READ); //store the current working directory using absolute path
+	// Serial.print("currentDir: ");
+	// size_t ptr = (size_t)(&currentDir);
+	// Serial.println(ptr);
+	// currentDir.printName(&Serial);
+	// Serial.println("<-");
+
+	while(file.openNext(&currentDir,O_READ)) //loop through the contents of currentDir
+	{
+		file.printName(&Serial); //print the file names
+
+		if (file.isDir()) {
+			// Indicate a directory.
+			Serial.write('/');
+
+			//recursively ls the directory entries
+			file.getName(lfn,LFN_LENGTH);
+			Serial.print("\nDirectory found: entering ");
+			Serial.println(lfn);
+			sd.chdir(lfn);
+			lsDir();
+			sd.chdir(path.c_str());
+		}
+		Serial.println();
+		file.close();
+
+		// //Print the address of the currentDir object
+		// //THIS VALUE IS CHANGING SOMEWHERE SOMEHOW
+		// Serial.print("currentDir: ");
+		// size_t ptr = (size_t)(&currentDir);
+		// Serial.println(ptr);
+		// currentDir.printName(&Serial);
+		// Serial.println("<-");
+
+	}
+
+	Serial.println("No files left in directory");
+
+	//restore the path to the parent directory
+	path.remove(pathLength);
+}
+
 void setup() {
+
 	path = "/";
+	SdFile file;
 	Serial.begin(baud);
 	while(!Serial);
 	Serial.println("Press the any key");
 	while(!Serial.available());
 
-	if (!sd.begin(sd_CS_PIN, SPI_HALF_SPEED)) {
+	if (!sd.begin(SD_CS_PIN, SPI_HALF_SPEED)) {
 		sd.initErrorHalt();
-}
-
-sd.ls();
-sd.chdir("Music");
-
-
-char test[160];
-file.getName(test,160);
-Serial.print("Test of LFN ")
-Serial.println(test); 
-
-
-char temp[13];
-sd.vwd()->getSFN(temp);
-path += temp;
-path += "/";
-Serial.println("Path is " + path);
-
-sd.ls();
-sd.chdir("Seth Ect - DiMethylTriptamine");
-
-sd.vwd()->getSFN(temp);
-path += temp;
-path += "/";
-Serial.println("Path is " + path);
-
-while(file.openNext(sd.vwd(),O_READ))
-{
-	Serial.println();
-	char test[160];
-	file.getName(test,160);
-	Serial.print("Test of LFN ");
-	Serial.println(test); 
-
-
-	file.printFileSize(&Serial);
-	Serial.write(' ');
-	file.printModifyDateTime(&Serial);
-	Serial.write(' ');
-	file.printName(&Serial);
-	
-	if (file.isDir()) {
-		// Indicate a directory.
-		Serial.write('/');
 	}
-	Serial.println();
-	file.close();
-}
-Serial.println("Done");
+
+	sd.chdir(path.c_str());
+	lsDir(true);
+	// sd.ls();
+	// sd.chdir("Music");
+
+	// char temp[160];
+	// sd.vwd()->getName(temp,160);
+	// path += temp;
+	// path += "/";
+	// Serial.println("Path is " + path);
+
+	// sd.ls();
+	// sd.chdir("Seth Ect - DiMethylTriptamine");
+
+	// sd.vwd()->getName(temp,160);
+	// path += temp;
+	// path += "/";
+	// Serial.println("Path is " + path);
+
+	// while(file.openNext(sd.vwd(),O_READ))
+	// {
+
+	// 	file.printFileSize(&Serial);
+	// 	Serial.write(' ');
+	// 	file.printModifyDateTime(&Serial);
+	// 	Serial.write(' ');
+	// 	file.printName(&Serial);
+		
+	// 	if (file.isDir()) {
+	// 		// Indicate a directory.
+	// 		Serial.write('/');
+	// 	}
+	// 	Serial.println();
+	// 	file.close();
+	// }
+	Serial.println("Done");
 }
 
 void loop() {}
