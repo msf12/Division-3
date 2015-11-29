173eae0dcbc656bd87421867d1c027089775b48b
msf12 <msf12@hampshire.edu>
2015-11-29 17:54:54 -0500

    Fleshing out skeleton with high level OS pieces

--- Teensy/Settings.h
+++ Teensy/Settings.h
@@ -5,7 +5,18 @@
 #include <string>
 
 #define SD_CS_PIN SS
-#define PREVIOUS_SONG_DELAY 1000
+
+//User Input pins
+#define PREVIOUS_SONG 10
+#define NEXT_SONG 10
+#define PREVIOUS_MENU_ITEM 10
+#define NEXT_MENU_ITEM 10
+#define PLAY_SELECT 10
+#define PREVIOUS_MENU 10
+#define SWITCH_VIEW_MODE 10
+
+const uint16_t PREVIOUS_SONG_DELAY = 1000;
+bool songView = false;
 
 typedef enum MenuTypeEnum {
 	MAIN_MENU,
@@ -15,19 +26,18 @@ typedef enum MenuTypeEnum {
 	ALBUM_MENU,
 	ALBUM_SONG_MENU,
 	SONG_MENU,
-	SONG_VIEW,
 	SETTINGS_MENU
 } MenuType;
 
-typedef enum ButtonEnum {
-	PREVIOUS_SONG,
-	NEXT_SONG,
-	PREVIOUS_MENU_ITEM,
-	NEXT_MENU_ITEM,
-	PLAY_SELECT,
-	PREVIOUS_MENU,
-	NOW_PLAYING
-} Button;
+// typedef enum ButtonEnum {
+// 	PREVIOUS_SONG,
+// 	NEXT_SONG,
+// 	PREVIOUS_MENU_ITEM,
+// 	NEXT_MENU_ITEM,
+// 	PLAY_SELECT,
+// 	PREVIOUS_MENU,
+// 	NOW_PLAYING
+// } Button;
 
 SdFat SD;
 MenuType menu;
--- Teensy/TopLevelSkeleton.ino
+++ Teensy/TopLevelSkeleton.ino
@@ -1,7 +1,7 @@
 #include "Settings.h"
 
 bool checkForInput();
-Button getInput();
+int getInput();
 void eraseScreen();
 void printToScreen(String s);
 void displayMenu(MenuType menu);
@@ -39,134 +39,141 @@ void setup()
 	}
 	printToScreen("Device has booted successfully\nLoading main menu");
 	menu = MAIN_MENU; //typedef enum MenuType
+
+	//User Input interrupts
+	attachInterrupt(PREVIOUS_SONG, previousSongInterrupt, LOW);
+	attachInterrupt(NEXT_SONG, nextSongInterrupt, LOW);
+	attachInterrupt(PREVIOUS_MENU_ITEM, previousItemInterrupt, LOW);
+	attachInterrupt(NEXT_MENU_ITEM, nextItemInterrupt, LOW);
+	attachInterrupt(PLAY_SELECT, playSelectInterrupt, LOW);
+	attachInterrupt(PREVIOUS_MENU, previousMenuInterrupt, LOW);
+	attachInterrupt(SWITCH_VIEW_MODE, switchViewInterrupt, LOW);
 }
 
 void loop()
 {
 	//print the current view to the screen
 	displayMenu(menu);
+}
 
-	//getInput waits until the input buffer is not empty
-	static Button pressed;
-	pressed = getInput(); //typedef enum Button
+//PREVIOUS SONG NEEDS DOUBLE CLICK FIXED
+void previousSongInterrupt()
+{
+	//two presses in PREVIOUS_SONG_DELAY milliseconds means previous song
+	//a single press means restart the current song
+	static unsigned long timer;
+	timer = millis();
 
-	//switches based on which button was pressed
-	switch (pressed) {
-
-		//if the previous song button was pressed
-		case PREVIOUS_SONG:
-
-			//two presses in PREVIOUS_SONG_DELAY milliseconds means previous song
-			//a single press means restart the current song
-			static unsigned long timer;
-			timer = millis();
-
-			//check for the PREVIOUS_SONG button to be pressed again within PREVIOUS_SONG_DELAY milliseconds
-			while(timer+PREVIOUS_SONG_DELAY < millis())
+	//check for the PREVIOUS_SONG button to be pressed again within PREVIOUS_SONG_DELAY milliseconds
+	while(timer+PREVIOUS_SONG_DELAY < millis())
+	{
+		//checks the input buffer for a button press
+		if(checkForInput())
+		{
+			//get the button that was pressed and update pressed
+			if(getInput() == PREVIOUS_SONG)
 			{
-				//checks the input buffer for a button press
-				if(checkForInput())
-				{
-					//get the button that was pressed and update pressed
-					if((pressed = getInput()) == PREVIOUS_SONG)
-					{
-						//if the button was PREVIOUS_SONG play the previous song
-						//if the previous song does not exist stop playing altogether
-						if(exists(previousSong))
-							playNewSong(previousSong);
-						else
-							stopPlaying();
-						break;
-					}
-				}
-			}
-			//if PREVIOUS_SONG was not pressed twice in time restart the current song
-			restartCurrentSong();
-			//if no second button was pressed break the switch statement
-			if(pressed == PREVIOUS_SONG)
+				//if the button was PREVIOUS_SONG play the previous song
+				//if the previous song does not exist stop playing altogether
+				if(exists(previousSong))
+					playNewSong(previousSong);
+				else
+					stopPlaying();
 				break;
-
-		//if the next song button was pressed
-		case NEXT_SONG:
-			//if the next song exists play it otherwise stop playing music altogether
-			if(nextSong != "")
-				playNewSong(nextSong);
-			else
-				stopPlaying();
-			break;
-
-		//if the previous menu item button was pressed
-		case PREVIOUS_MENU_ITEM:
-			//if the current view displays a menu
-			if(menu != SONG_VIEW)
-			{	
-				//if there is a previous menu item update which item is selected
-				if(exists(previousItem))
-					selectedItem = previousItem;
 			}
-			//else do nothing
-			break;
-
-		//if the next menu item button was pressed
-		case NEXT_MENU_ITEM:
-			//if the current view displays a menu
-			if(menu != SONG_VIEW)
-			{
-				//if there is a next menu item update which item is selected
-				if(exists(nextItem))
-					selectedItem = nextItem;
-			}
-			//else do nothing
-			break;
-
-		//if the play/select button was pressed
-		case PLAY_SELECT:
-			//if the current view is the song view, pause the current song
-			if(menu == SONG_VIEW)
-			{
-				pauseSong();
-			}
-			//else if the current view is a list of songs play the selected new song
-			//if the song is the same as the current song it will continue to play
-			else if(menu == ARTIST_SONG_MENU || menu == ALBUM_SONG_MENU || menu == SONG_MENU)
-			{
-				playNewSong(selectedItem);
-			}
-			//else interpret the play button as the menu item select button and change to the selected submenu
-			else
-			{
-				changeMenu(selectedItem);
-			}
-			break;
-		//if the previous menu button was pressed
-		case PREVIOUS_MENU:
-			//if the current view is the song view, change to the appropriate menu view
-			if(menu == SONG_VIEW)
-			{
-				changeToMenuView(previousMenu);
-			}
-			//else change to the menu one directory higher than the current menu
-			else if(menu != MAIN_MENU)
-			{
-				changeMenu(previousMenu);
-			}
-			break;
-		//if the now playing button was pressed
-		case NOW_PLAYING:
-			//if the current view is the song view, switch to the most recently viewed menu
-			if(menu == SONG_VIEW)
-			{
-				changeToMenuView();
-			}
-			//else switch to song view
-			else
-			{
-				changeToSongView();
-			}
-			break;
-		//default means an error has occured
-		default:
-			eraseScreen();
-			printToScreen("Button Error: unknown button pressed");
+		}
 	}
+	//if PREVIOUS_SONG was not pressed twice in time restart the current song
+	restartCurrentSong();
+}
+
+void nextSongInterrupt()
+{
+	noInterrupts();
+	//if the next song exists play it otherwise stop playing music altogether
+	if(nextSong != "")
+		playNewSong(nextSong);
+	else
+		stopPlaying();
+	interrupts();
+}
+
+void previousItemInterrupt()
+{
+	noInterrupts();
+	//if the current view displays a menu
+	if(!songView)
+	{	
+		//if there is a previous menu item update which item is selected
+		if(exists(previousItem))
+			selectedItem = previousItem;
+	}
+	interrupts();
+}
+
+void nextItemInterrupt()
+{
+	noInterrupts();
+	//if the current view displays a menu
+	if(!songView)
+	{
+		//if there is a next menu item update which item is selected
+		if(exists(nextItem))
+			selectedItem = nextItem;
+	}
+	interrupts();
+}
+
+void playSelectInterrupt()
+{
+	noInterrupts();
+	//if the current view is the song view, pause the current song
+	if(songView)
+	{
+		pauseSong();
+	}
+	//else if the current view is a list of songs play the selected new song
+	//if the song is the same as the current song it will continue to play
+	else if(menu == ARTIST_SONG_MENU || menu == ALBUM_SONG_MENU || menu == SONG_MENU)
+	{
+		playNewSong(selectedItem);
+	}
+	//else interpret the play button as the menu item select button and change to the selected submenu
+	else
+	{
+		changeMenu(selectedItem);
+	}
+	interrupts();
+}
+
+void previousMenuInterrupt()
+{
+	noInterrupts();
+	//if the current view is the song view, change to menu view
+	if(songView)
+	{
+		changeToMenuView();
+	}
+	//else change to the menu one directory higher than the current menu
+	else if(menu != MAIN_MENU)
+	{
+		changeMenu(previousMenu);
+	}
+	interrupts();
+}
+
+void switchViewInterrupt()
+{
+	noInterrupts();
+	//if the current view is the song view, switch to the most recently viewed menu
+	if(songView)
+	{
+		changeToMenuView();
+	}
+	//else switch to song view
+	else
+	{
+		changeToSongView();
+	}
+	interrupts();
 }
