31262abdd8b0b7716ec2c8f70e8802283d2d385a
msf12 <msf12@hampshire.edu>
2016-02-05 20:07:17 -0500

    DCaS homework1

--- Digital Circuits and Systems/Homework/homework1.py
+++ Digital Circuits and Systems/Homework/homework1.py
@@ -1,14 +1,20 @@
-def AB():
-	pass
+def figureA(A,B,C,D,E):
+	return (A and B) or (C and (D or E))
 
-def CD():
-	pass
+def figureB(A,B,C,D,E):
+	return (A and B) or (C and D) or (C and E)
+	
+def main():
+    print( "  A  B  C  D  E  |  f  g" )
+    print( "-----------------+---------" )
+    for A in [ 0, 1 ]:
+        for B in [ 0, 1 ]:
+            for C in [ 0, 1 ]:
+                for D in [ 0, 1 ]:
+                    for E in [ 0, 1 ]:
+                        print( "%3d%3d%3d%3d%3d  |%3d%3d" % 
+                         ( A, B, C, D, E,
+                         figureA( A, B, C, D, E ),
+                         figureB( A, B, C, D, E ) ) )
 
-def CE():
-	pass
-
-def figureA():
-	pass
-
-def figureB():
-	pass
+main()
