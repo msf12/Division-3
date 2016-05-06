d3e3efceb9bc1401541490fb0fb2b571cf6c2bc8
Mitchel Fields <msf12@hampshire.edu>
2016-05-05 21:37:28 -0400

    DCaS Final Lab Portion
    Code is now written for the Arduino Diecimila and the 6811 processor such that
    the 6811 will output an oscillating signal at a speed dependent on a value
    stored at memory address 0000 and the Arduino will light a single LED for each
    of the three possible frequencies or no LEDs if the program is not running. The
    program is commented and a video has been taken of it running, but a lab report
    must still be written and circuit diagrams for the 4-10 decoder, NOT gate, and
    D Flip-Flop that make up the hardware driver must be created.

--- Digital Circuits and Systems/Final/problem2.asm
+++ Digital Circuits and Systems/Final/problem2.asm
@@ -1,55 +1,69 @@
 	ORG	0000
-a	DB	0
+a	DB	0 ;0 = fast, 1 = medium, 2 = slow
 
 	ORG	0010
-	LDAA	a
-	CMPA	#1
-	BLO	less
-	BHI	more
+	LDAA	a ;load frequency
+	CMPA	#1 ;compare to 1
+	BLO	less ;if frequency is less than 1 jump to less
+	BHI	more ;if frequency is more than 1 jump to more
 
+;if the frequency option is 1: output ACCA, increment it, store it on the stack,
+;wait one second, pop ACCA from the stack, and jump to equal
 equal:
 	STAA	8000
 	INCA
-	JMP	loop
+	PSHA
+	JSR	C027
+	PULA
+	JMP	equal
 
+;if the frequency option is 0: output ACCA, increment it, and jump to less
 less:
-	STAA	8000
-	ADDA	#2
-	SUBA	#1
 	STAA	8000
 	INCA
 	JMP	less
 
+;if the frequency option is 2: output ACCA, increment it, store it on the stack,
+;wait one second, pop ACCA from the stack, push ACCA back onto the stack,
+;wait one second, pop ACCA from the stack, and jump to more
+;the middle pop and push are to ensure that ACCA remains at the top of the stack
+;after two JSR commands
 more:
 	STAA	8000
 	INCA
-	STAA	8000
-	ADDA	#2
-	SUBA	#1
+	PSHA
+	JSR	C027
+	PULA
+	PSHA
+	JSR	C027
+	PULA
 	JMP	more
 
-;change this byte to change the oscillation pattern
+;change this byte to change the oscillation frequency
 0000	00
 ...
 0010	96 00
 0012	81 01
-0014	21 0A;[offset to less]
-0016	22 15;[offset to more]
+0014	25 0E;[offset to less]
+0016	22 13;[offset to more]
 ;equal
 0018	B7 8000
-001A	4C
-001C	7E 0018
+001B	4C
+001C	36
+001D	BD C027
+0020	32
+0021	7E 0018
 ;less
-001F	B7 8000
-0022	8B 02
-0024	80 01
-0026	B7 8000
-0029	4C
-002A	7E 001F
+0024	B7 8000
+0027	4C
+0028	7E 0024
 ;more
-002D	B7 8000
-0030	4C
-0031	B7 8000
-0034	8B 02
-0036	80 01
-0038	7E 002D
+002B	B7 8000
+002E	4C
+002F	36
+0030	BD C027
+0033	32
+0034	36
+0035	BD C027
+0038	32
+0039	7E 0029
--- Digital Circuits and Systems/Final/problem2/problem2.ino
+++ Digital Circuits and Systems/Final/problem2/problem2.ino
@@ -1,9 +1,75 @@
+#define DEBUG 0 // set to 1 for serial debugging output
+
+#define PIN_6811 3
+#define OUTPIN_1 8
+#define OUTPIN_2 9
+#define OUTPIN_3 10
+
+uint32_t pulseLength = 0, val = 0; //the length of time that the pulse read stayed HIGH
+bool updatePulseLength = true; //whether pulseLength needs to be updated this loop
+
 void setup()
 {
-	
+	pinMode(PIN_6811, INPUT);
+	pinMode(OUTPIN_1, OUTPUT);
+	pinMode(OUTPIN_2, OUTPUT);
+	pinMode(OUTPIN_3, OUTPUT);
+
+#if DEBUG
+	Serial.begin(9600);
+#endif DEBUG
 }
 
 void loop()
 {
-	
+	// pulseIn courtesy of http://www.benripley.com/diy/arduino/three-ways-to-read-a-pwm-signal-with-arduino/
+	val = pulseIn(PIN_6811, HIGH, 10000000);
+
+#if DEBUG
+	Serial.println(val);
+#endif
+
+	// this makes sure that only the first pulse is saved when the signal changes
+	if(val && updatePulseLength)
+	{
+		pulseLength = val;
+		updatePulseLength = false;
+	}
+	else if(!val && !updatePulseLength)
+	{
+		updatePulseLength = true;
+		pulseLength = 0;
+	}
+
+#if DEBUG
+	Serial.println("Pulse length: " + String(pulseLength));
+#endif
+
+	// these are the pulse-length buckets used to determine which pulse is being sent to the arduino
+	// the actual pulse-length possibilities are in much smaller ranges, but the buckets are left large
+	// to ensure accuracy in the outputs
+	if(pulseLength == 0)
+	{
+		digitalWrite(OUTPIN_1, LOW);
+		digitalWrite(OUTPIN_2, LOW);
+		digitalWrite(OUTPIN_3, LOW);
+	}
+	else if(pulseLength < 1000000)
+	{
+		digitalWrite(OUTPIN_1, HIGH);
+		digitalWrite(OUTPIN_2, LOW);
+		digitalWrite(OUTPIN_3, LOW);
+	}
+	else if(pulseLength < 3000000)
+	{
+		digitalWrite(OUTPIN_1, LOW);
+		digitalWrite(OUTPIN_2, HIGH);
+		digitalWrite(OUTPIN_3, LOW);
+	}
+	else
+	{
+		digitalWrite(OUTPIN_1, LOW);
+		digitalWrite(OUTPIN_2, LOW);
+		digitalWrite(OUTPIN_3, HIGH);
+	}
 }
